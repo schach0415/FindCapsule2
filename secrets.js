5e53e66e135f78dea163cdd811d82e659c503507
Dongwoo Kang <schach0415@yahoo.com>
2018-04-09 21:34:48 -0400

    test before secrets file

---
+++ secrets.js
@@ -0,0 +1,6 @@
+process.env.REACT_APP_FIREBASE_KEY = ''
+process.env.REACT_APP_AUTH_DOMAIN = ''
+process.env.REACT_APP_DATABASE_URL = ''
+process.env.REACT_APP_PROJECT_ID = ''
+process.env.REACT_APP_STORAGE_BUCKET = ''
+process.env.REACT_APP_MESSAGING_SENDER_ID = ''
