b91a6d527fc98c6418b86206b0813b2f327f1612
Dongwoo Kang <schach0415@yahoo.com>
2018-04-09 21:37:36 -0400

    test before secrets file3

--- secrets.js
+++ secrets.js
@@ -4,3 +4,6 @@ process.env.REACT_APP_DATABASE_URL = ''
 process.env.REACT_APP_PROJECT_ID = ''
 process.env.REACT_APP_STORAGE_BUCKET = ''
 process.env.REACT_APP_MESSAGING_SENDER_ID = ''
+
+
+anything
